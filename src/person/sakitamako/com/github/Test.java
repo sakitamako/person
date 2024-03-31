47ff42a5b4e50d321181a00b2ca0c63b26f7fe71
sakitamako <sa.919.1025.ki@gmail.com>
2024-03-31 14:18:47 +0900

    7行目編集

--- src/person/sakitamako/com/github/Test.java
+++ src/person/sakitamako/com/github/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] agrs) {
 		Person taro = new Person();
 		taro.name = "山田太郎";
-		taro.age =20;
+		taro.age = 20;
 		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
