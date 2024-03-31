129f14a2bd7c8443d5a68d4e2f705f635265304b
sakitamako <sa.919.1025.ki@gmail.com>
2024-03-31 14:15:27 +0900

    演習１追加

--- src/person/sakitamako/com/github/Person.java
+++ src/person/sakitamako/com/github/Person.java
@@ -1,5 +1,6 @@
 package person.sakitamako.com.github;
 
 public class Person {
-
+	public String name = null;
+	public int age = 0;
 }
--- src/person/sakitamako/com/github/Test.java
+++ src/person/sakitamako/com/github/Test.java
@@ -1,5 +1,13 @@
 package person.sakitamako.com.github;
 
 public class Test {
+	public static void main(String[] agrs) {
+		Person taro = new Person();
+		taro.name = "山田太郎";
+		taro.age =20;
+		
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+	}
 
 }
