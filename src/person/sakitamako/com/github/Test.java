c4f2972f66f88e1281b1e6888a66107bd5220e4b
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-01 20:48:21 +0900

    演習5.6の追加

--- src/person/sakitamako/com/github/Test.java
+++ src/person/sakitamako/com/github/Test.java
@@ -8,11 +8,13 @@ public class Test {
 		taro.phoneNumber = "090-0000-0000";
 		taro.address = "taro@gmail.com";
 		
-		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
 		System.out.println(taro.phoneNumber);
 		System.out.println(taro.address);
+		taro.talk();
+		taro.walk();
+		taro.run();
 		
 		Person jiro = new Person();
 		jiro.name = "木村次郎";
@@ -24,6 +26,9 @@ public class Test {
 		System.out.println(jiro.age);
 		System.out.println(jiro.phoneNumber);
 		System.out.println(jiro.address);
+		jiro.talk();
+		jiro.walk();
+		jiro.run();
 		
 		Person hanako = new Person();
 		hanako.name = "鈴木花子";
@@ -35,6 +40,9 @@ public class Test {
 		System.out.println(hanako.age);
 		System.out.println(hanako.phoneNumber);
 		System.out.println(hanako.address);
+		hanako.talk();
+		hanako.walk();
+		hanako.run();
 		
 		Person saki = new Person();
 		saki.name = "鈴木紗季";
@@ -46,6 +54,9 @@ public class Test {
 		System.out.println(saki.age);
 		System.out.println(saki.phoneNumber);
 		System.out.println(saki.address);
+		saki.talk();
+		saki.walk();
+		saki.run();
 	}
 
 }
